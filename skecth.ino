f0b1666fa6cb41912f14ca2c367c6b167cab91f6
himanshu17890 <himanshupawar259@gmail.com>
2026-07-20 13:55:57 +0530

    updating project

---
+++ skecth.ino
@@ -0,0 +1,11 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>  
+
+// oled configurations 
+# define screen_size 128
+# define screen_width 64
+# define oled_rst -1
+
+//ring buffer configuration 
+# define ringbuffer_size 16
